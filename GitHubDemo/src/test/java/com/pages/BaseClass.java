9aac3ad8d04a65c3455a833e7d19ff0376650d92
your_username <dconcio@yondu.com>
2023-03-07 16:49:01 +0800

    - Parameterized xmlFiles and browser values in pom.xml

--- GitHubDemo/src/test/java/com/pages/BaseClass.java
+++ GitHubDemo/src/test/java/com/pages/BaseClass.java
@@ -6,8 +6,12 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 
@@ -16,12 +20,23 @@ public class BaseClass {
 	public WebDriver driver;
 
 	@BeforeTest
-	public void setup() {
+	@Parameters({"guru99Url"})
+	public void setup(@Optional("Abc") String guru99Url) {
 
-		WebDriverManager.chromedriver().clearResolutionCache().setup();
-		driver = new ChromeDriver();
-		driver.get("http://demo.guru99.com/test/guru99home/");
+		if (System.getProperty("browser").equalsIgnoreCase("Chrome")) {
+			WebDriverManager.chromedriver().clearResolutionCache().setup();
+			driver = new ChromeDriver();
+
+		} else if (System.getProperty("browser").equalsIgnoreCase("Firefox")) {
+			WebDriverManager.firefoxdriver().clearResolutionCache().setup();
+			driver = new FirefoxDriver();
+		} else if (System.getProperty("browser").equalsIgnoreCase("Edge")) {
+			WebDriverManager.edgedriver().clearResolutionCache().setup();
+			driver = new EdgeDriver();
+		}
+
 		driver.manage().window().maximize();
+		driver.get(guru99Url);
 	}
 
 	@AfterTest
