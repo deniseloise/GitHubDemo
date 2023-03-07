d1f2063dde8e205cf1236b5c90a85787ef9a0af4
your_username <dconcio@yondu.com>
2023-03-07 16:57:40 +0800

    Created SampleTest2 and testng_github2.xml file

---
+++ GitHubDemo/src/test/java/com/testcases/SampleTest2.java
@@ -0,0 +1,13 @@
+package com.testcases;
+
+import org.testng.annotations.Test;
+
+public class SampleTest2 {
+
+	@Test
+	public void TC_4() {
+		System.out.println("This is Test Case # 4");
+
+	}
+	
+}
